ef07d89d8f411c740ddeb7303830ef97f6321c1c
Jefferson Firmino Fernandes <jeff.firmino72@gmail.com>
2024-06-19 20:08:05 -0300

    navigation buttons

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,4 +1,4 @@
-import {View, Text, ScrollView} from "react-native";
+import {View, Text, ScrollView, StyleSheet, TouchableOpacity} from "react-native";
 import {Link, useNavigation} from "expo-router";
 import {useEffect} from "react";
 import Routes from "../routes";
@@ -8,6 +8,8 @@ import HeaderHome from "@/components/headerHome";
 import SliderHome from "@/components/sliderHome";
 import ArticlesHome from "@/components/articlesHome";
 import SustainabilityTipsScreen from "@/components/tipsHome";
+import ProductScreen from "./productScreen";
+import { fontFamily } from "../styles/fontFamily";
 
 const Home: React.FC = () => {
   const navigation = useNavigation();
@@ -19,35 +21,65 @@ const Home: React.FC = () => {
     });
   }, []);
 
-  return(
+  return (
     <ScrollView>
       <View>
         <View>
-          <HeaderHome/>
+          <HeaderHome />
         </View>
         <View>
-          <SliderHome/>
+          <SliderHome />
         </View>
         <View>
           <ScrollView>
-            <SustainabilityTipsScreen/>
-            <ArticlesHome/>
+            <SustainabilityTipsScreen />
+            <ArticlesHome />
           </ScrollView>
         </View>
-        <View>
-          
+        <View style={styles.buttonContainer}>
+          <Link href="/calculatorScreen" style={styles.button}>
+            <Text style={styles.buttonText}>Calculadora</Text>
+          </Link>
+          <Link href="/productScreen" style={styles.button}>
+            <Text style={styles.buttonText}>Produtos</Text>
+          </Link>
         </View>
-        <Text>----</Text>
-        <Link href={"/calculatorScreen"}>Ir para a calculadora de pegada de carbono</Link>
-        <Text>----</Text>
-        <Link href={"/productScreen"}>Ir para a página de produtos</Link>
       </View>
     </ScrollView>
-    
-    /*<NavigationContainer>
-      <Routes/>
-    </NavigationContainer>*/
   );
-}
+};
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginVertical: 20,
+    paddingHorizontal: 10,
+  },
+  button: {
+    flex: 1,
+    backgroundColor: '#D3A9F4',
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    borderRadius: 30,
+    borderWidth: 1.5,
+    borderColor: '#000',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5,
+    marginHorizontal: 10,
+    textAlign: 'center',
+    fontFamily: fontFamily.pmedium,
+    
+  },
+  buttonText: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
 
 export default Home;
